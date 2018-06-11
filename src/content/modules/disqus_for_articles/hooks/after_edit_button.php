5f237ec9e6087e4017f0a8a244a83c896b47dd07
Ulrich Schmidt <support@ulicms.de>
2018-06-12 00:10:00 +0200

    disqus-comments: funktioniert nun auch mit https://

--- src/content/modules/disqus_for_articles/hooks/after_edit_button.php
+++ src/content/modules/disqus_for_articles/hooks/after_edit_button.php
@@ -10,20 +10,20 @@ if (get_type () == "article") {
 
     // The following are highly recommended additional parameters. Remove the slashes in front to use.
     var disqus_identifier = '<?php echo md5(Settings::get("password_salt").get_ID());?>';
-    // var disqus_url = 'http://example.com/permalink-to-page.html';
+    // var disqus_url = 'https://example.com/permalink-to-page.html';
 
     /* * * DON'T EDIT BELOW THIS LINE * * */
     (function() {
         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+        dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     })();
 </script>
 <noscript>
 	Please enable JavaScript to view the <a
-		href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a>
+		href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a>
 </noscript>
-<a href="http://disqus.com" class="dsq-brlink">blog comments powered by
+<a href="https://disqus.com" class="dsq-brlink">blog comments powered by
 	<span class="logo-disqus">Disqus</span>
 </a>
 
